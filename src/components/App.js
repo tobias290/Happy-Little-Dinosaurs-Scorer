6f5bc82c970d305a5c5b4b5337ee34f4b04aedf2
Toby Essex <tobysx@googlemail.com>
2022-06-13 21:26:00 +0100

    .

---
+++ src/components/App.js
@@ -0,0 +1,45 @@
+import "../css/app.css";
+
+const CHARACTERS = [
+    {
+        name: "Bad Luck Bronto",
+        color: "purple",
+        image: "bad-luck-bronto.png",
+    },
+    {
+        name: "Cry Ceratops",
+        color: "red",
+        image: "cry-ceratops.png",
+    },
+    {
+        name: "Nervous Rex",
+        color: "yellow",
+        image: "nervous-rex.png",
+    },
+    {
+        name: "Pterry Dactyl",
+        color: "pink",
+        image: "pterry-dactyl.png",
+    },
+    {
+        name: "Raging Raptor",
+        color: "blue",
+        image: "raging-raptor.png",
+    },
+    {
+        name: "Stego",
+        color: "green",
+        image: "stego.png",
+    }
+];
+
+
+function App() {
+    return (
+        <div id="test">
+            Testing
+        </div>
+    );
+}
+
+export default App;
